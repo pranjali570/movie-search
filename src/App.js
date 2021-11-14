5c4bc09d536aac7072e5c6f1725353c457b2849c
pranjali570 <pranjali.nagal@gmail.com>
2021-11-14 13:30:16 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  let [movieInfo, setMovieInfo] = useState("The Avengers");
+  let [title, setTitle] = useState("The Avengers");
+
+  useEffect(() => {
+      getMovieData();
+    }, [])
+
+    function readTitle(value) {
+      setTitle(value)
+    }
+
+    function getMovieData() {
+      let url = `http://www.omdbapi.com/?t=${title}&apikey=928c896f`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((movie) => {
+          console.log(movie)
+          setMovieInfo(movie)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="padd-search">
+          <h1>Search Movie Details</h1>
+          <div className="search-group">
+            <input type="text" placeholder="Enter Movie" onChange={(event) => {readTitle(event.target.value)}} className="movie-search"/>
+            <button className="btn" onClick={getMovieData}>Get Movie</button>
+          </div>
+          {
+            movieInfo?.Error === undefined ? (
+              <div className="movie">
+                <div className="poster">
+                  <img src={movieInfo?.Poster} className="img-poster" alt="Movie Poster" />
+                </div>
+                <div className="details">
+                  <div className="padd">
+                    <h1>{movieInfo?.Title}</h1>
+                    <p><strong>Genre</strong>: {movieInfo?.Genre}</p>
+                    <p><strong>Directed by</strong>: {movieInfo?.Director}</p>
+                    <p><strong>Plot</strong>: {movieInfo?.Plot}</p>
+                    <p><strong>Cast</strong>: {movieInfo?.Actors}</p>
+                    <p><strong>BoxOffice</strong>: {movieInfo?.BoxOffice}</p>
+                    <p><strong>Language</strong>: {movieInfo?.Language}</p>
+                    <p><strong>Released</strong>: {movieInfo?.Released}</p>
+                    <p><strong>Runtime</strong>: {movieInfo?.Runtime}</p>
+                    <p><strong>Rated</strong>: {movieInfo?.Rated}</p>
+                    
+                    {/* <div className="movie-ratings">
+                      {
+                        movieInfo?.Ratings.map((rating, index) => (
+                          <div key={index}>
+                            <strong>{rating.Source}</strong>
+                            <h3>{rating.Value}</h3>
+                          </div>
+                        ))
+                      }
+                    </div>   */}
+                    
+                </div>
+              </div>
+            </div>
+            ) : 
+            (
+              <h1>Movie Not Found</h1>
+            )
+          }
+
+        </div>
+     
+      </div>
     </div>
   );
 }
